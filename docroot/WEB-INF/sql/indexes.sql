245ca4da2b5f515e814c649d036f0f9ee9f375dc
Florencia Gadea <florencia@rotterdam-cs.com>
2013-01-09 13:41:20 -0300

    added new portlet to display in front-end

--- docroot/WEB-INF/sql/indexes.sql
+++ docroot/WEB-INF/sql/indexes.sql
@@ -1,3 +1,2 @@
-create index IX_BFC7FF13 on GoogleAnalytics_Configuration (property);
-create index IX_6C52A5E on GoogleAnalytics_Configuration (propertyname);
-create index IX_2B72309C on GoogleAnalytics_Configuration (propertyname, groupId, companyId);
+create index IX_E3059467 on configuration_Configuration (propertyname);
+create index IX_2EBA3C25 on configuration_Configuration (propertyname, groupId, companyId);
